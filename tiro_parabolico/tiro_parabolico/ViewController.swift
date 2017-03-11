43a1372de41e38d5f9d723f3b5cfc4974763a5b8
Marco Peyrot <mpeyrotc@dhcp-10-16-177-245.mty.itesm.mx>
2017-03-11 17:49:17 -0600

    first commit

---
+++ tiro_parabolico/tiro_parabolico/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tiro_parabolico
+//
+//  Created by Marco Peyrot on 3/11/17.
+//  Copyright © 2017 Marco Peyrot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
