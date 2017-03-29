62cc419d88d6cca417eee1f9267ecbb442a05481
Daniel Gonzalez <danielgonzalez@Daniels-MacBook-Pro.local>
2017-03-29 10:39:16 -0600

    Grafica con curva, bug con el ancho de la grafica.

--- tiro_parabolico/tiro_parabolico/ViewController.swift
+++ tiro_parabolico/tiro_parabolico/ViewController.swift
@@ -10,30 +10,37 @@ import UIKit
 import Charts
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var yVelocityLabel: UILabel!
     @IBOutlet weak var distanceLabel: UILabel!
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
-    
     @IBOutlet weak var lineChartView: LineChartView!
     
-    var yAxis: [Int]!
-    var lineChartDataSet = LineChartDataSet()
-    var lineChartData = LineChartData()
-    var index:Int = -1
+    var yAxis = [Double]()
+    var xAxis = [Double]()
+    var index:Int = 1
     var shot: Shot!
-    
     var initialVelocity: Double!
     var startX: Double!
     var startY: Double!
     var angle: Double!
+    var valorPrevio:Double = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        yAxis = []
-        setChart(yValues: yAxis)
+        
+        // Generamos nuestro objeto "Shot" con los valores enviados
         shot = Shot(initialVelocity, startX, startY, angle)
+        
+        // Asignamos el primer punto en tiempo 0
+        yAxis.append(shot.yForTime(0)!)
+        xAxis.append(shot.xForTime(0)!)
+        
+        // Pintamos la grafica con el primer punto en tiempo 0
+        setChart()
+        
+        // Imprimimos la informacion del objeto "shot" con tiempo 0
         yVelocityLabel.text = String(format: "%.2f m/s", shot.getYVelocity(0))
         distanceLabel.text = String(format: "%.2f m", shot.xForTime(0)!)
         heightLabel.text = String(format: "%.2f m", shot.yForTime(0)!)
@@ -46,44 +53,128 @@ class ViewController: UIViewController {
     }
 
 
-    func setChart(yValues: [Int]){
+    func setChart(){
         
         var dataInput: [ChartDataEntry] = []
         
-        for i in 0..<yValues.count {
-            let dataEntry = ChartDataEntry(x: Double(i), y: Double(yValues[i]))
+        // Introducimos los puntos
+        for i in 0..<yAxis.count {
+            let dataEntry = ChartDataEntry(x: xAxis[i], y: yAxis[i])
             dataInput.append(dataEntry)
         }
         
-        lineChartDataSet = LineChartDataSet(values: dataInput, label: "Altura")
-        lineChartDataSet.circleRadius = 2
-        lineChartDataSet.circleColors = [NSUIColor.black]
-        lineChartDataSet.lineWidth = 5
-        lineChartDataSet.colors = [NSUIColor.black]
-        lineChartDataSet.drawValuesEnabled = false
-        lineChartData = LineChartData(dataSet: lineChartDataSet)
-        lineChartView.data = lineChartData
-        lineChartView.animate(xAxisDuration: 1)
+        // Establecemos el estilo de nuestra grafica
+        let chartDataSet = LineChartDataSet(values: dataInput, label: "Altura")
+        chartDataSet.circleRadius = 2
+        chartDataSet.circleColors = [NSUIColor.black]
+        chartDataSet.lineWidth = 5
+        chartDataSet.colors = [NSUIColor.black]
+        //––––––––––––––OPCIONAL LINEA PUNTEADA
+        //chartDataSet.colors = [NSUIColor.clear]
+        chartDataSet.drawValuesEnabled = false
         
+        //Cargamos el estilo de la grafica al set de datos
+        var dataSet: [IChartDataSet] = []
+        dataSet.append(chartDataSet)
+        
+        //Creamos la grafica con el set de datos
+        let chartData = LineChartData(dataSets: dataSet)
+        
+        //Se la asignamos al view para que muetre la grafica.
+        lineChartView.data = chartData
+        lineChartView.chartDescription!.text = ""
+        
+        
+        //Formato de la grafica.
+        
+        //Formato del eje izquiero
+        lineChartView.leftAxis.axisMinimum = 0.0
+        lineChartView.leftAxis.axisMaximum = shot.yForTime(shot.finalTime()/2)! + 1
+        
+        //Formato del eje derecho
+        lineChartView.rightAxis.enabled = false
+        
+        //Formato del eje inferior
+        lineChartView.xAxis.axisMinimum = shot.getInitialXPos()
+        lineChartView.xAxis.axisMaximum = shot.xForTime(shot.finalTime())!
+        
+
     }
     
     @IBAction func update(_ sender: UIStepper) {
-        let value = Int(sender.value)
+        // Valor del stepper
+        let value = sender.value
         
-        if yAxis.count < value {
-            yAxis.append(value - 1)
-            index += 1
-        } else {
-            yAxis.removeLast()
-            index -= 1
+        /* 
+         Valor del ultimo segundo posible antes de tocar 0 en altura
+         el "ceil" es para darle margen a la grafica y que se pueda
+         apreciar mejor
+        */
+        let segundoFinal = ceil(shot.finalTime())
+    
+        /* 
+         Si el valor del stepper es menor o igual al ultimo segundo
+         podemos pintar mas puntos
+        */
+        if value <= segundoFinal {
+            
+            /*
+             Si el valor del stepper incremento pintamos el siguiente segundo
+            */
+            if value > valorPrevio {
+                
+                /*
+                 Este ciclo sirve para pintar 10 puntos entre el segundo
+                 "x" y el segundo "y" inclusivos
+                */
+                for index in stride(from: valorPrevio, to: value, by: 0.1){
+                    yAxis.append(shot.yForTime(Double(index))!)
+                    xAxis.append(shot.xForTime(Double(index))!)
+                }
+                
+                //Actualizamos el valor previo
+                valorPrevio = value
+                
+            } else if value < valorPrevio {
+                
+                /*
+                 Este ciclo es para eliminar los ultimos 10 puntos pintados
+                */
+                for _ in 1...10 {
+                    yAxis.removeLast()
+                    xAxis.removeLast()
+                }
+                
+                //Actualizamos el valor previo
+                valorPrevio = value
+            }
         }
-        setChart(yValues: yAxis)
+
+        //Pintamos la nueva grafica
+        setChart()
         
-        // Shot info
-        yVelocityLabel.text = String(format: "%.2f m/s", shot.getYVelocity(sender.value))
-        distanceLabel.text = String(format: "%.2f m", shot.xForTime(sender.value)!)
-        heightLabel.text = String(format: "%.2f m", shot.yForTime(sender.value)!)
-        timeLabel.text = String(format: "%.2f s", sender.value)
+        // Informacion del objeto shot
+        
+        /*
+         Si el stepper se pasa del ultimo segundo, no redondeado, 
+         Entonces imprimimos la informacion en el ultimo segundo cuando llega
+         a  y = 0
+         ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+         Si no, entonces imprimimos los valores en el tiempo que el stepper nos
+         marque
+         */
+        if value > shot.finalTime() {
+            timeLabel.text = String(format: "%.2f s", shot.finalTime())
+            yVelocityLabel.text = String(format: "%.2f m/s", shot.getYVelocity(shot.finalTime()))
+            distanceLabel.text = String(format: "%.2f m", shot.xForTime(shot.finalTime())!)
+            heightLabel.text = String(format: "%.2f m", shot.yForTime(shot.finalTime())!)
+            sender.value = segundoFinal
+        } else {
+            timeLabel.text = String(format: "%.2f s", sender.value)
+            yVelocityLabel.text = String(format: "%.2f m/s", shot.getYVelocity(sender.value))
+            distanceLabel.text = String(format: "%.2f m", shot.xForTime(sender.value)!)
+            heightLabel.text = String(format: "%.2f m", shot.yForTime(sender.value)!)
+        }
     }
     
 }
