fac7f665ef4f067f45df2d9cf0a400e606d1571b
Jose Luis Carvajal <josecarcar1@gmail.com>
2017-04-24 10:46:01 -0500

    Added class Gradiente

---
+++ tiro_parabolico/tiro_parabolico/Gradiente.swift
@@ -0,0 +1,37 @@
+//
+//  Gradiente.swift
+//  tiro_parabolico
+//
+//  Created by José Luis Carvajal Carbajal on 4/24/17.
+//  Copyright © 2017 Marco Peyrot. All rights reserved.
+//
+
+import UIKit
+
+class Gradiente: UIView {
+
+    @IBInspectable var startColor: UIColor = UIColor.blue // modificar
+    @IBInspectable var endcolor: UIColor = UIColor.green // modificar
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+        
+        let contexto = UIGraphicsGetCurrentContext()
+        
+        let colores = [startColor.cgColor, endcolor.cgColor]
+        
+        let espacioColor = CGColorSpaceCreateDeviceRGB()
+        
+        let localizacionColor: [CGFloat] = [0.0, 1.0]
+        
+        let gradiente = CGGradient(colorsSpace: espacioColor, colors: colores as CFArray, locations: localizacionColor)
+        
+        let puntoInicio = CGPoint.zero
+        let puntoFin = CGPoint(x: 0, y: self.bounds.height)
+        contexto?.drawLinearGradient(gradiente!, start: puntoInicio, end: puntoFin, options: CGGradientDrawingOptions.drawsAfterEndLocation)
+    }
+
+
+}
--- tiro_parabolico/tiro_parabolico/InicioViewController.swift
+++ tiro_parabolico/tiro_parabolico/InicioViewController.swift
@@ -10,10 +10,35 @@ import UIKit
 
 class InicioViewController: UIViewController {
 
+    @IBOutlet weak var btnPreguntas: UIButton!
+    @IBOutlet weak var btnSimulacion: UIButton!
+    @IBOutlet weak var btnAcerca: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        // setupGradient()
+        
+        let colorBg: UIColor = UIColor(red: 1, green: 0, blue: 0, alpha: 0.9)
+        
+        // Background color for buttons
+        btnPreguntas.layer.backgroundColor = colorBg.cgColor
+        btnSimulacion.layer.backgroundColor = colorBg.cgColor
+        btnAcerca.layer.backgroundColor = colorBg.cgColor
+        
+        // Border for buttons
+        btnPreguntas.layer.borderWidth = 1
+        btnPreguntas.layer.borderColor = colorBg.cgColor
+        btnPreguntas.layer.cornerRadius = 5
+        btnSimulacion.layer.borderWidth = 1
+        btnSimulacion.layer.borderColor = colorBg.cgColor
+        btnSimulacion.layer.cornerRadius = 5
+        btnAcerca.layer.borderWidth = 1
+        btnAcerca.layer.borderColor = colorBg.cgColor
+        btnAcerca.layer.cornerRadius = 5
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +46,19 @@ class InicioViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+
+    /*func setupGradient() {
+        // Background color for view
+        let colorTop = UIColor(red: 0, green: 0.2, blue: 1, alpha: 1)
+        let colorBottom = UIColor(red: 0, green: 0.4, blue: 1, alpha: 0.7)
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.view.bounds
+        gradientLayer.colors = [colorTop, colorBottom]
+        gradientLayer.locations = [0.0, 1.0]
+        
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
+    }*/
 
     
     // MARK: - Navigation
