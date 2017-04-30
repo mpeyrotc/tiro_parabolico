0d4c1dbc4847880bc4dd0382b1a95f381df9632e
Marco Peyrot <mpeyrotc@dhcp-10-16-177-217.mty.itesm.mx>
2017-04-29 21:23:08 -0500

    Add pdf viewer

---
+++ tiro_parabolico/tiro_parabolico/PDFViewController.swift
@@ -0,0 +1,47 @@
+//
+//  PDFViewController.swift
+//  tiro_parabolico
+//
+//  Created by Marco Peyrot on 4/29/17.
+//  Copyright © 2017 Marco Peyrot. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class PDFViewController: UIViewController {
+    @IBOutlet weak var pdfView: UIWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        let url = Bundle.main.url(forResource: "hoz", withExtension: "pdf")
+    
+        if let url = url {
+            
+            let urlRequest = NSURLRequest(url: url)
+            pdfView.loadRequest(urlRequest as URLRequest)
+            
+            view.addSubview(pdfView)
+        }
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
